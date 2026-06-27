5a6a86840a86047d8fe75d3ea2c1266bcb08a827
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2026-06-27 20:09:50 -0300

    import TargetDetection + delete Raycast Collisions
    - Raycast Collision no logró tener uso. El algoritmo de Princess Tower es mucho más eficiente.
    - Importado TargetDetection de DProject.

---
+++ Assets/Scripts/Gameplay/Enemy/TargetDetection.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TargetDetection : MonoBehaviour
+{
+    public UnityEvent<Transform[]> TargetUpdate;
+    public UnityEvent<Transform> TargetFound;
+    public UnityEvent<Transform> TargetLost;
+
+    List<Transform> targets = new List<Transform>();
+
+    public Transform[] Targets { get => targets.ToArray(); }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(targets.Contains(other.transform)) { return; }
+        targets.Add(other.transform);
+        TargetFound?.Invoke(other.transform);
+        TargetUpdate?.Invoke(targets.ToArray());
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        targets.Remove(collision.transform);
+        TargetLost?.Invoke(collision.transform);
+        TargetUpdate?.Invoke(targets.ToArray());
+    }
+}
